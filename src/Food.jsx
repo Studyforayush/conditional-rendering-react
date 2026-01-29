3e14db2b73fe3982602ccecd154d7eb47e9fc26b
Ayush Dhakad <studyfroayush@gmail.com>
2026-01-29 22:10:58 +0530

    new

---
+++ src/Food.jsx
@@ -0,0 +1,22 @@
+
+function Food(){
+
+    const food1 = "Munchurion"
+    const food2 = "Chowmin"
+    const MyPrice = [20,70,60]
+
+    return(
+        <div>
+            <ul>
+                <li>Pasta</li>
+                <li>{food1}</li>
+                <li>{food2}</li>
+                <li>{MyPrice}</li>
+
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Food;
